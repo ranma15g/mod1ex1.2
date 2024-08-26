317411363ba464c986b8bc65e87a8638ed1d5498
ranma15g <ranma15g@gmail.com>
2024-08-26 00:44:28 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+/*
+
+Class: COP 2800: Java Programming
+Instructor: Francis Fiskey
+Description: 1.2 (Display five messages) Write a program that displays Welcome to Java five times. 
+Due: Sep 8 by 11:59pm
+I pledge by honor that I have completed the programming assignment independently.
+I have not copied the code from a student or any source.
+I have not given my code to any student.
+Sign here: ___Eric Snyder___
+
+*/
+
+// We define the public class Main to start the program
+public class Main { // This brace opens our class block
+
+  // We introduce the main method to run the program 
+  public static void main(String[] args) {
+        // This brace opens our main method block
+
+        // We print our first line
+        System.out.println("Welcome to Java");
+
+        // We print our second line
+        System.out.println("Welcome to Java");
+    
+        // We print our third line
+        System.out.println("Welcome to Java");
+    
+        // We print our fourth line
+        System.out.println("Welcome to Java");
+    
+        // We print our fifth line
+        System.out.println("Welcome to Java");
+    
+        
+
+    } // This brace closes the main method block
+
+} // This brace closes the class block
+
+// And that's all folks!
